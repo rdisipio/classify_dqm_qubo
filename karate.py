3b7d1bf6a70a5e33b5418af9a51c1f9d65262244
Riccardo Di Sipio <riccardo.di.sipio@cern.ch>
2021-06-21 14:43:34 -0400

    vars better names

--- karate.py
+++ karate.py
@@ -11,26 +11,25 @@ from random import random
 G = nx.karate_club_graph()
 
 dqm = dimod.DiscreteQuadraticModel()
-num_partitions = 4
-num_nodes = G.number_of_nodes()
-lagrange = 3
+n_clusters = 4
+n_nodes = G.number_of_nodes()
+beta = 3
 
 for i in G.nodes:
-    dqm.add_variable(num_partitions, label=i)
+    dqm.add_variable(n_clusters, label=i)
 
-constraint_const = lagrange * (1 - (2 * num_nodes / num_partitions))
-
+norm_factor = beta * (1 - (2 * n_nodes / n_clusters))
 for i in G.nodes:
-    linear_term = constraint_const + (0.5 * np.ones(num_partitions) * G.degree[i])
+    linear_term = norm_factor + (0.5 * np.ones(n_clusters) * G.degree[i])
     dqm.set_linear(i, linear_term)
 
 # Quadratic term for node pairs which do not have edges between them
 for p0, p1 in nx.non_edges(G):
-    dqm.set_quadratic(p0, p1, {(c, c): (2 * lagrange) for c in range(num_partitions)})
+    dqm.set_quadratic(p0, p1, {(c, c): (2 * beta) for c in range(n_clusters)})
 
 # Quadratic term for node pairs which have edges between them
 for p0, p1 in G.edges:
-    dqm.set_quadratic(p0, p1, {(c, c): ((2 * lagrange) - 1) for c in range(num_partitions)})
+    dqm.set_quadratic(p0, p1, {(c, c): ((2 * beta) - 1) for c in range(n_clusters)})
 
 sampler = LeapHybridDQMSampler()
 sampleset = sampler.sample_dqm(dqm, label='Example - Graph Partitioning DQM')
@@ -38,7 +37,7 @@ sampleset = sampler.sample_dqm(dqm, label='Example - Graph Partitioning DQM')
 sample = sampleset.first.sample
 energy = sampleset.first.energy
 
-color_list = [(random(), random(), random()) for i in range(num_partitions)]
+color_list = [(random(), random(), random()) for i in range(n_clusters)]
 color_map = [color_list[sample[i]] for i in G.nodes]
 nx.draw(G, node_color=color_map)
 plt.savefig('graph_partition_result.png')
